9436252c96cf13d261f8c8e8724e9aab8bd08159
Ali Shakoor <ali.shakoor2249@gmail.com>
2021-12-20 20:31:31 -0500

    Made some formatting changes to the files.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-11/11/2021
-
-generate password.py program generates a random password of eight characters. Each time the program runs a new password is generated 
-utilzing random. The password has two upper case letters, two lower case letters, two digits between 0 and 9, and 2 special characters.
+This program generates a random password of eight characters. Each time the program runs a new password is generated utilzing random. The password has two upper case letters, two lower case letters, two digits between 0 and 9, and 2 special characters.
--- generate_password.py
+++ generate_password.py
@@ -1,25 +1,28 @@
 import random
 
-#A function do shuffle all the characters of a string
+def generate_string():
+  uppercaseLetter1=chr(random.randint(65,90)) 
+  uppercaseLetter2=chr(random.randint(65,90)) 
+  lowercase_letter_one=chr(random.randint(97,122)) 
+  lowercase_letter_two=chr(random.randint(97,122)) 
+  first_number=random.randint(0,9) 
+  second_number=random.randint(0,9) 
+  first_symbol=chr(random.randint(33,63)) 
+  second_symbol=chr(random.randint(33,63)) 
+
+  generated_string = uppercaseLetter1 + uppercaseLetter2 + lowercase_letter_one + lowercase_letter_two + str(first_number) + str(second_number)+ first_symbol + second_symbol
+
+  return generated_string
+
 def shuffle(string):
   tempList = list(string)
   random.shuffle(tempList)
   return ''.join(tempList)
 
-#Main program starts here
-uppercaseLetter1=chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-uppercaseLetter2=chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-lowercase_letter_one=chr(random.randint(97,122)) #Generate a random lowercase letter based on the ASCII code)
-lowercase_letter_two=chr(random.randint(97,122)) #Generate a random lowercase letter based on the ASCII code)
-first_number=random.randint(0,9) #Generate a random number.
-second_number=random.randint(0,9) #Generate a random number
-first_symbol=chr(random.randint(33,63)) #Generate a random symbol
-second_symbol=chr(random.randint(33,63)) #Generate a random symbol
+def main():
+  pw=shuffle(generate_string())
+  print(pw)
 
 
-#Generate password using all the characters, in random order
-password = uppercaseLetter1 + uppercaseLetter2 + lowercase_letter_one + lowercase_letter_two + str(first_number) + str(second_number)+ first_symbol + second_symbol # + ....
-password = shuffle(password)
-
-#Ouput
-print(password)
+if __name__ == "__main__":
+    main()
