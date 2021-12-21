9b67413f8a74527789e8caee2d1e48c8ad9fec9b
Ali Shakoor <ali.shakoor2249@gmail.com>
2021-12-21 00:01:16 -0500

    Made improvements and changes to the program. Open README.md for more information.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 This program generates a random password of eight characters. Each time the program runs a new password is generated utilzing random. The password has two upper case letters, two lower case letters, two digits between 0 and 9, and 2 special characters.
+
+Major changes was made to the program. A class, PasswordManager was defiend to better organize the program.
+
+The creation of the random passowrd was condensed into one method called generate_pw
+
+THe csv module was imported in order to create a csv file where the generated passwords along with their associated application could be stored.
+
+The user is able to create as many passwords as they want utilizng a while loop in main.
--- generate_password.py
+++ generate_password.py
@@ -1,27 +1,47 @@
+import csv
 import random
 
-def generate_string():
-  uppercaseLetter1=chr(random.randint(65,90)) 
-  uppercaseLetter2=chr(random.randint(65,90)) 
-  lowercase_letter_one=chr(random.randint(97,122)) 
-  lowercase_letter_two=chr(random.randint(97,122)) 
-  first_number=random.randint(0,9) 
-  second_number=random.randint(0,9) 
-  first_symbol=chr(random.randint(33,63)) 
-  second_symbol=chr(random.randint(33,63)) 
+class PasswordManager:
+  def __init__(self):
+    self.csv_path="pw.csv"
+  
+  def generate_pw(self):
+    uppercaseLetter1=chr(random.randint(65,90)) 
+    uppercaseLetter2=chr(random.randint(65,90)) 
+    lowercase_letter_one=chr(random.randint(97,122)) 
+    lowercase_letter_two=chr(random.randint(97,122)) 
+    first_number=random.randint(0,9) 
+    second_number=random.randint(0,9) 
+    first_symbol=chr(random.randint(33,63)) 
+    second_symbol=chr(random.randint(33,63)) 
 
-  generated_string = uppercaseLetter1 + uppercaseLetter2 + lowercase_letter_one + lowercase_letter_two + str(first_number) + str(second_number)+ first_symbol + second_symbol
+    generated_string = uppercaseLetter1 + uppercaseLetter2 + lowercase_letter_one + lowercase_letter_two + str(first_number) + str(second_number)+ first_symbol + second_symbol
 
-  return generated_string
-
-def shuffle(string):
-  tempList = list(string)
-  random.shuffle(tempList)
-  return ''.join(tempList)
+    tempList = list(generated_string)
+    random.shuffle(tempList)
+    return ''.join(tempList)
+  
+  def save_password(self, application_name, pw):
+    with open(self.csv_path, 'a', newline='') as file:
+      password_writer=csv.writer(file, delimiter=',')
+      password_writer.writerow([application_name, pw])
+    return True
 
 def main():
-  pw=shuffle(generate_string())
-  print(pw)
+  password_manager=PasswordManager()
+  while True:
+    print("enter 'q' anytime to exit...")
+    if(application_name := input("Enter the name of the application you want to create a password for: "))=='q':
+      break
+
+    pw=password_manager.generate_pw()
+    print(f'New password for "{application_name}" is {pw}')
+
+    if (response := input("Would you like to save this new password? (y/n): "))=='y':
+      password_manager.save_password(application_name, pw)
+      print(f'{pw} saved successully.')
+    elif response=='q':
+      break
 
 
 if __name__ == "__main__":
